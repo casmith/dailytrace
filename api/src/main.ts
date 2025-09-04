7f1e5dcfa9d3ff64069b3a449cdede88a905d825
Clay Smith <casmith@users.noreply.github.com>
2025-09-04 00:49:14 -0500

    add api prefix

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('api');
   const port = process.env.PORT || 8080;
   await app.listen(port);
   // eslint-disable-next-line no-console
